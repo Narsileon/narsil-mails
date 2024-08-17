7a9d0f8dfb05ca84d9258f6df9ab7e4a98435517
Narsilien <rigaux.jonathan@gmail.com>
2024-08-17 11:25:56 +0200

    Add menus

---
+++ src/Http/Menus/Menu.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Narsil\Auth\Http\Menus;
+
+#region USE
+
+use Narsil\Menus\Http\Menus\AbstractMenu;
+use Narsil\Menus\Models\MenuNode;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ *
+ * @author Jonathan Rigaux
+ */
+class Menu extends AbstractMenu
+{
+    #region PUBLIC METHODS
+
+    /**
+     * @return array
+     */
+    public static function getBackendMenu(): array
+    {
+        return [[
+            MenuNode::LABEL => 'Mailers',
+            MenuNode::URL => '/backend/mailers',
+            MenuNode::RELATIONSHIP_ICON => 'lucide/send',
+        ], [
+            MenuNode::LABEL => 'Email signatures',
+            MenuNode::URL => '/backend/email-signatures',
+            MenuNode::RELATIONSHIP_ICON => 'lucide/signature',
+        ], [
+            MenuNode::LABEL => 'Email templates',
+            MenuNode::URL => '/backend/email-templates',
+            MenuNode::RELATIONSHIP_ICON => 'lucide/mail',
+        ]];
+    }
+
+    #endregion
+}
